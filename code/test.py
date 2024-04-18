4209c737fdefa0b124e704cc18ccd29f1edd92c3
lupohan44 <cptf_lupohan@126.com>
2024-04-18 17:55:44 -0400

    Add multithread support for scrapper

--- code/test.py
+++ code/test.py
@@ -7,8 +7,9 @@ def main():
     # prompt = 'What is the meaning of life?'
     # print(gemini.generate(prompt).text)
     # https://ai.google.dev/tutorials/python_quickstart
-    scrapper = Scrapper('https://i.stack.imgur.com/Ccnnp.png')
-    print(scrapper.get_content())
+    scrapper = Scrapper(['https://i.stack.imgur.com/Ccnnp.png', 'https://i.stack.imgur.com/Ccnnp2.png'])
+    scrapper.get_all_contents(thread_num=1)
+    print(scrapper.result_map)
 
 
 if __name__ == '__main__':
--- code/utils/Scrapper.py
+++ code/utils/Scrapper.py
@@ -1,22 +1,34 @@
+import threading
+
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 
 class Scrapper:
     def __init__(self, url):
-        self.url = url
+        self.url = set()
+        if type(url) == str:
+            self.url.add(url)
+        else:
+            for u in url:
+                self.url.add(u)
+        self.result_map = {}
+        self.progress_bar = None
 
-    def get_content(self):
+    def get_one_content(self, url):
         resp = None
         try:
-            resp = requests.get(self.url)
+            resp = requests.get(url)
             if not resp.ok:
-                return {'url': self.url, 'content': None, 'http_status': resp.status_code,
-                        'error': None, 'success': False}
+                self.result_map[url] = {'url': url, 'content': None, 'http_status': resp.status_code,
+                                        'error': None, 'success': False}
+                return self.result_map[url]
             content_type = resp.headers.get('Content-Type', '')
             if 'text/html' not in content_type:
-                return {'url': self.url, 'content': None, 'http_status': resp.status_code,
-                        'error': 'Non-HTML content', 'success': False}
+                self.result_map[url] = {'url': url, 'content': None, 'http_status': resp.status_code,
+                                        'error': 'Non-HTML content', 'success': False}
+                return self.result_map[url]
             resp_text = resp.text
             soup = BeautifulSoup(resp_text, 'html.parser')
             for script in soup(["script", "style", "img"]):
@@ -26,7 +38,39 @@ class Scrapper:
             import re
             text = re.sub(r'\s+', ' ', text)
 
-            return {'url': self.url, 'content': text, 'http_status': resp.status_code, 'error': None, 'success': True}
+            self.result_map[url] = {'url': url, 'content': text, 'http_status': resp.status_code, 'error': None,
+                                    'success': True}
+            return self.result_map[url]
         except Exception as e:
-            return {'url': self.url, 'content': None, 'http_status': resp.status_code,
-                    'error': str(e), 'success': False}
+            self.result_map[url] = {'url': url, 'content': None, 'http_status': resp.status_code, 'error': str(e),
+                                    'success': False}
+            return self.result_map[url]
+
+    def get_contents_thread(self):
+        while True:
+            url = None
+            try:
+                url = self.url.pop()
+            except:
+                pass
+            if url is None:
+                return
+            try:
+                self.get_one_content(url)
+                if self.progress_bar is not None:
+                    self.progress_bar.update(1)
+            except:
+                pass
+
+    def get_all_contents(self, thread_num=5, show_progress=True):
+        threads = []
+        if show_progress:
+            self.progress_bar = tqdm(total=len(self.url), desc="Scraping progress", ncols=100)
+        for _ in range(thread_num):
+            thread = threading.Thread(target=self.get_contents_thread)
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+        if show_progress:
+            self.progress_bar.close()
